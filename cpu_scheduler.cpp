399fb6e5801d4be8e3cbc3d7a273c57c327637a3
Amon <amonmoce@yahoo.fr>
2014-10-28 01:51:10 +0800

    Readfile done

--- cpu_scheduler.cpp
+++ cpu_scheduler.cpp
@@ -1,9 +1,24 @@
+//#include "cpu_scheduler.h"
 #include "text_processing.h"
-
+#include<fstream>
+#include<iostream>
+using namespace std;
 int main(){
+  const string filename = "testdata/testdata1.txt";
 
-  read_file_by_line("test.txt");
-  write_in_the_file();
+  if(file_exist(filename))
+    text_to_array(filename);
+
+
+  typedef struct process{
+    string name;
+    int arrival_time, burst_time;
+  }Process;
+
+//  for(int i=0; i<word_count; i++)
+  //  cout<<words[i];
+
+  //write_in_the_file();
 
   return 0;
 }
--- text_processing.h
+++ text_processing.h
@@ -1,21 +1,35 @@
 #ifndef TEXT_PROCESSING_H
 #define TEXT_PROCESSING_H
   #include <fstream>
-  //iostream just for tests purposes
-  #include <iostream>
   using namespace std;
 
-  void read_file_by_line(string filename){
+  const int max_number_of_words = 100;
+  int word_count = 0;
+  string words[max_number_of_words];
 
-    cout<<"the file is "<<filename<<endl;
-
-
-
+  bool file_exist(string filename){
+    ifstream datafile;
+    datafile.open(filename);
+    if(datafile.is_open())
+      return true;
+    else
+      return false;
   }
+  
+  void text_to_array(string filename){
+    ifstream datafile;
+    datafile.open(filename);
+    if(datafile.is_open()){
+      while(!datafile.eof()){
+          datafile >> words[word_count++];
+      }
+    }
+  }
+
 
   void write_in_the_file(){
 
-    cout<<"wrote in the file"<<endl;
+  //  cout<<"wrote in the file"<<endl;
 
   }
 
