61176889d0e06cd6a383cb7eae05c172df9cebaf
Amon <amonmoce@yahoo.fr>
2014-10-27 21:50:31 +0800

    Organisation start

---
+++ cpu_scheduler.cpp
@@ -0,0 +1,9 @@
+#include "text_processing.h"
+
+int main(){
+
+  read_file_by_line("test.txt");
+  write_in_the_file();
+
+  return 0;
+}
---
+++ text_processing.h
@@ -0,0 +1,22 @@
+#ifndef TEXT_PROCESSING_H
+#define TEXT_PROCESSING_H
+  #include <fstream>
+  //iostream just for tests purposes
+  #include <iostream>
+  using namespace std;
+
+  void read_file_by_line(string filename){
+
+    cout<<"the file is "<<filename<<endl;
+
+
+
+  }
+
+  void write_in_the_file(){
+
+    cout<<"wrote in the file"<<endl;
+
+  }
+
+#endif
